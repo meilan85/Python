734005bca77db67ab67799f8110d3b3e9205c096
ilan-tmp <ilan.m1000@gmail.com>
2017-11-12 19:20:18 +0200

    add list delimiter

--- List/List.py
+++ List/List.py
@@ -1,4 +1,9 @@
-nums = [1,2,3,4,5]
-print(max(nums))
-print(min(nums))
-print(sum(nums))
+# nums = [1,2,3,4,5]
+# print(max(nums))
+# print(min(nums))
+# print(sum(nums))
+
+courses = ["history","math","phsics","compssci"]
+delimiter = ' '
+courses_delimiter = delimiter.join(courses)
+print(courses_delimiter)
